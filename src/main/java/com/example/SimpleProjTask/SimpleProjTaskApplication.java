5459b4db989722150f2451d0cd48e4d6aca22168
ali.taleshi <ali.taleshi@nak-mci.ir>
2025-01-01 15:32:37 +0330

    Initial commit

---
+++ src/main/java/com/example/SimpleProjTask/SimpleProjTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleProjTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProjTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProjTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimpleProjTask/SimpleProjTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleProjTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
